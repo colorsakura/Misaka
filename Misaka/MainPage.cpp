84545c3af89cc0af46b5faf5d3ab4f23f56a1c8e
Chauncey <2268136725@qq.com>
2020-05-22 12:08:44 +0800

    init

---
+++ Misaka/MainPage.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "MainPage.h"
+#include "MainPage.g.cpp"
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+namespace winrt::Misaka::implementation
+{
+    MainPage::MainPage()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainPage::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainPage::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
